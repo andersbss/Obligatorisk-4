57a578e27299c172be59fb062abaa7aa3b3b128f
andersbss <43789401+andersbss@users.noreply.github.com>
2020-09-14 14:29:59 +0200

    2

--- index.js
+++ index.js
@@ -20,19 +20,19 @@ try {
 
   /// let (or const) ///
 
-  // (function UseLetOrConst() {
-  //   // Use let (or const) instead of var to make the function succeed.
-  //   var x = 1;
-  //   if (true) {
-  //     var x = 2;
-  //   }
+  (function UseLetOrConst() {
+    // Use let (or const) instead of var to make the function succeed.
+    const x = 1;
+    if (true) {
+      const x = 2;
+    }
 
-  //   // Don't make changes below this line
+    // Don't make changes below this line
 
-  //   expect(x).toBe(1);
+    expect(x).toBe(1);
 
-  //   solved++;
-  // })();
+    solved++;
+  })();
 
   /// Template strings ///
 
